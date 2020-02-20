7cfd1454845c653dbfc6635353edeb88709b9e94
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2020-02-21 01:55:44 +0200

    [init]

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,18 @@ import Head from '../components/head';
 import { db } from '../dbconfig'
 
 const Home = () => {
-    const [names, setNames] = useState(null);
 
     useEffect(() => {
-        db.collection("notes").onSnapshot(snap => {
-            setNames(snap.docs.map(doc => doc.data()))
-        })
+        
     }, []);
 
     return (
-        <div>
-            <style jsx>{`
-                .test{
-                    color: red;
-                }
-            `}</style>
+        <>
             <Head title="Home" />
-            <div className="test">ADasdasdasd</div>
-            {names
-                ?
-                names.map((name, i) => (
-                    <p key={i}>{name.name}</p>
-                ))
-                :
-                "loading..."
-            }
-        </div>
+            <style jsx>{`
+
+            `}</style>
+        </>
     );
 };
 
