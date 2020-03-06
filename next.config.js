b0f5eeb0976ebe6692404454398d876c440ca8b8
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-03-06 15:27:14 +0200

    [Update] Env variables with Now Secrets

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  env: {
-    firebase_auth_domain: process.env.firebase_auth_domain,
-    firebase_database_url: process.env.firebase_database_url,
-    firebase_project_id: process.env.firebase_project_id,
-    firebase_public_api_key: process.env.firebase_public_api_key,
-  },
+    env: {
+        firebase_auth_domain: process.env.firebase_auth_domain,
+        firebase_database_url: process.env.firebase_database_url,
+        firebase_project_id: process.env.firebase_project_id,
+        firebase_public_api_key: process.env.firebase_public_api_key
+    }
 }
