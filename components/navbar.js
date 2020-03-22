91d54b36c71b5b4040350c728acf29cbff385f27
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-03-23 00:48:57 +0200

    [Update] Navbar small syntax error fix

--- components/navbar.js
+++ components/navbar.js
@@ -48,15 +48,12 @@ const Navbar = props => {
                     <h1>Aripis</h1>
                 </div>
                 <div className="navbar-buttons">
-                    {AuthUser ? (
+                    {AuthUser &&
                         <>
                             <Link href="/addproduct">
                                 <a>Add Product</a>
                             </Link>
                         </>
-                    ) : (
-                        <></>
-                    )
                     }
                     <Link href="/">
                         <a>Home</a>
