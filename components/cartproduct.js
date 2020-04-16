cc2052a73bdc729663d63c67472d0a678c20ba62
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2020-04-17 00:01:58 +0300

    [Update] created CartProduct component, linked with db and created basic logic

--- components/cartproduct.js
+++ components/cartproduct.js
@@ -1,15 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import initFirebase from '../utils/initFirebase'
+
+initFirebase()
 
 const CartProduct = props => {
+  const [product, setProduct] = useState([])
+
+  useEffect(() =>{
+    // console.log(props.id)
+    firebase.firestore().collection("products").doc(props.dbId).get().then(doc => {
+      setProduct(doc.data())
+      console.log(doc.data())
+    })
+  }, [])
+
   return (
     <>
       <style jsx>{`
-        .wrp:nth-child(odd) {
+        .wrp {
           color: red;
+          width: 20px;
+          height: 20px;
+        }
+
+        img {
+          object-fit: contain;
+          width: 50px;
+          height: 50px;
         }
       `}</style>
-      <div className="wrp">
-        <h5>id: {props.dbId}</h5><h5>quantity: {props.quantity}</h5>
+      <div className="wrp" >
+        <img src={product.urls && product.urls[0]} />
+        <p>name: {product.name}</p>
+        <p>quantity: {props.quantity}</p>
+        <p>price: {product.price * props.quantity}</p>
       </div>
     </>
   )
