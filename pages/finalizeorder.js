08d164df639ea738cc6555ba4dd63a208cda17cd
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-04-26 15:50:38 +0300

    [Update] Order finalization

--- pages/finalizeorder.js
+++ pages/finalizeorder.js
@@ -6,8 +6,8 @@ import Button from '../components/button';
 import Navbar from '../components/navbar';
 import Router from 'next/router';
 import CartContext from '../contexts/cartContext';
-import CartProduct from '../components/cartproduct';
-
+import OrderProduct from '../components/orderproduct';
+import priceConvert from '../utils/priceConvert';
 import withAuthUser from '../utils/pageWrappers/withAuthUser';
 import withAuthUserInfo from '../utils/pageWrappers/withAuthUserInfo';
 import firebase from 'firebase/app';
@@ -22,6 +22,7 @@ const FinalizeOrder = props => {
     const AuthUser = get(AuthUserInfo, 'AuthUser', null)
     const { cartContext, setCartContext } = useContext(CartContext)
     const [cart, setCart] = useState([])
+    const [totalPrice, setTotalPrice] = useState()
 
     const [billingAddress, setBillingAddress] = useState("")
     const [deliveryAddress, setDeliveryAddress] = useState("")
@@ -47,11 +48,14 @@ const FinalizeOrder = props => {
     useEffect(() => {
         if (AuthUser) {
             firebase.firestore().collection(`users/${AuthUser.id}/cart`).onSnapshot(cart => {
-                setCart(cart.docs.map(doc => ({
-                    ...doc.data(),
-                    id: doc.id
-                })))
-                setCartContext(false)
+                let cart_data = {}
+                let price = 0
+                cart.docs.forEach(doc => {
+                    cart_data[doc.id] = doc.data()
+                    price += doc.data().price * doc.data().quantity
+                })
+                setCart(cart_data)
+                setTotalPrice(price)
             })
             firebase.firestore().collection("users").doc(AuthUser.id).get().then(doc => {
                 setBillingAddress(doc.data().orderInfo.billingAddress)
@@ -75,38 +79,78 @@ const FinalizeOrder = props => {
 
     return (
         <>
-            <style jsx>{`
-                .wrp {
+             <style jsx>{`
+                .wrp-cartcontent {
+                    flex:1;
                     display: flex;
                     flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: #E9EBEE;
                 }
+                
+                .wrp-cartcontent > .cartcontent-productslist {
+                    background-color: white;
+                    border-radius: .3em;
+                    box-shadow: 0 .065em .19em rgba(0,0,0,0.12), 0 .065em .125em rgba(0,0,0,0.24);
+                    margin-right: 1em;
+                }
+
+                .wrp-cartcontent > .cartcontent-productslist > :global(:last-child) {
+                    border: 0;
+                }
+
+                .wrp-cartcontent > .cartcontent-info {
+                    padding: 1em 2em;
+                    display: flex;
+                    flex-direction: column;
+                    background-color: white;
+                    box-shadow: 0 .065em .19em rgba(0,0,0,0.12), 0 .065em .125em rgba(0,0,0,0.24);
+                    border-radius: .3em;
+                    margin-left: 1em;
+                }
+
+                .wrp-cartcontent > .cartcontent-info > .info-totalprice {
+                    font-weight: bold;
+                    font-size: 1.5em;
+                }
+
             `}</style>
             <Navbar {...props} />
-            <div className="wrp" >
-                <div className="cartcontent-productslist">
-                    {cart && cart.map((item, i) => (
-                        <CartProduct key={i}
-                            dbId={item.id}
-                            productId={item.productId}
-                            quantity={item.quantity}
-                            authId={AuthUser ? AuthUser.id : null}>
-
-                        </CartProduct>
-                    ))}
+            <div className="wrp-cartcontent">
+                {cart && Object.keys(cart).length > 0
+                    ?
+                    <>
+                        <div className="cartcontent-productslist">
+                            {
+                                Object.keys(cart).map(key => (
+                                    <OrderProduct
+                                        key={key}
+                                        dbId={key}
+                                        quantity={cart[key].quantity}
+                                        productId={cart[key].productId}
+                                        authId={AuthUser ? AuthUser.id : null}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <div className="cartcontent-info">
+                            <span className="info-totalprice">Total price: {priceConvert(totalPrice, "лв.")}</span>
+                            <span>{billingAddress}</span>
+                            <span>{deliveryAddress}</span>
+                            <span>{telephoneNumber}</span>
+                            <span>{orderType}</span>
+                            <span>{paymentMethod}</span>
+                            <Button onClick={handleSubmit}>
+                                Submit order
+                            </Button>
+                        </div>
+                    </>
+                    :
+                    <div>
+                        Your cart is empty :)
                 </div>
-                <div className="cartcontent-address">
-                    <p>{billingAddress}</p>
-                    <p>{deliveryAddress}</p>
-                    <p>{telephoneNumber}</p>
-                    <p>{orderType}</p>
-                    <p>{paymentMethod}</p>
-
-                </div>
-                <div className="cartcontent-button">
-                    <Button onClick={handleSubmit}>
-                        Submit Order
-                    </Button>
-                </div>
+                }
             </div>
         </>
     )
